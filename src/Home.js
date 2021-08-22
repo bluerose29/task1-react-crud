9af3ca968ccb9144811e9c4dd2b5306176e64b3e
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-08-22 13:57:46 +0800

    preventDefault on form ( remove reload ) and fixes

--- src/Home.js
+++ src/Home.js
@@ -1,51 +1,75 @@
-import addTask from './assets/img/AddIcon.png';
-import useFetch from './DataFetch';
-import TaskList from './TaskList';
-import { useState, useEffect } from 'react';
+import addTask from "./assets/img/AddIcon.png";
+import useFetch from "./DataFetch";
+import TaskList from "./TaskList";
+import { useState, useEffect } from "react";
 
 const Home = () => {
-    const {data: tasks, isPending, error} = useFetch('http://localhost:8000/tasks'); //npx json-server --watch data/db.json --port 8000
-    const [title, setTitle] = useState('');
+  const {
+    data: tasksFetch,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/tasks"); //npx json-server --watch data/db.json --port 8000
+  const [title, setTitle] = useState("");
+  const [tasks, setTasks] = useState([]);
 
-    const handleSubmit = (e) =>{
-        const task = {title};
-        console.log(task);
+  useEffect(() => {
+    setTasks(tasksFetch);
+  }, [tasksFetch]);
 
-        fetch('http://localhost:8000/tasks', {
-            method:'POST',
-            headers: {"Content-Type":"application/json"},
-            body: JSON.stringify(task)
-        }).then(() =>{
-            console.log("New Task")
-            console.log({title})
-        })
-    } 
+  const handleSubmit = (e) => {
+    const task = { title };
+    console.log(task);
 
-    useEffect(() => {
-        console.log('Site Running')
-    },[title]);
-    return ( 
-        <div className="home">
-            <form >
-            <div className="title">
-                <h2>Todo List</h2>
-                <button onClick={handleSubmit}><img src={addTask} alt="Add"/></button>
-            </div>
-            <div className="task-list-display">
-                {error && <div>{ error }</div>}
-                {isPending && <div style={{margin:"15px"}}>Loading Task...</div>}
-                {tasks && <TaskList tasks= {tasks}/>}
-            </div>
-            
-            <input type="text"
-                placeholder='New Task'
-                value ={title}
-                onChange = {(e) => setTitle(e.target.value)}
-            />
-            <p>{title}</p>
-            </form>
+    fetch("http://localhost:8000/tasks", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(task),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((resJson) => {
+        setTasks((prevState) => [...prevState, resJson]);
+
+        /**
+         * clear input
+         */
+        setTitle("");
+      });
+  };
+
+  useEffect(() => {
+    console.log("Site Running");
+  }, [title]);
+  return (
+    <div className="home">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <div className="title">
+          <h2>Todo List</h2>
+          <button onClick={handleSubmit}>
+            <img src={addTask} alt="Add" />
+          </button>
         </div>
-     );
-}
- 
+        <div className="task-list-display">
+          {error && <div>{error}</div>}
+          {isPending && <div style={{ margin: "15px" }}>Loading Task...</div>}
+          {tasks && <TaskList setTasks={setTasks} tasks={tasks} />}
+        </div>
+
+        <input
+          type="text"
+          placeholder="New Task"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <p>{title}</p>
+      </form>
+    </div>
+  );
+};
+
 export default Home;
--- src/TaskList.js
+++ src/TaskList.js
@@ -1,66 +1,77 @@
-import deleteIcon from './assets/img/DeleteIcon.png';
-import {useState} from 'react';
+import deleteIcon from "./assets/img/DeleteIcon.png";
+import { useState } from "react";
 
-const TaskList = ({tasks, title}) => {
-    const [checkedState, setCheckedState] = useState(
-        new Array(tasks.length).fill(false)
-      );
+/**
+ *
+ * @param {{tasks: {title: string, id: number}[] , title: string, setTasks: ()=> void}}
+ * @returns
+ */
+const TaskList = ({ tasks, title, setTasks }) => {
+  const [checkedState, setCheckedState] = useState(
+    new Array(tasks.length).fill(false)
+  );
 
-    function deleteTask(id){
-        console.log(id);
-        fetch(`http://localhost:8000/tasks/${id}`,{
-            method: 'DELETE'
-        }).then((result) =>{
-            result.json().then((resp) => {
-                console.warn(resp)
-            })
-        })   
-    }
-    const [count] = useState(Object.keys(tasks).length);
+  function deleteTask(id) {
+    fetch(`http://localhost:8000/tasks/${id}`, {
+      method: "DELETE",
+    })
+      .then((result) => {
+        return result.json();
+      })
+      .then(() => {
+        setTasks((prevState) => prevState.filter((filter) => filter.id !== id));
+      })
+      .catch((err) => {
+        console.error(err);
+        throw new Error(err);
+      });
+  }
+  const [count] = useState(Object.keys(tasks).length);
 
-    const [total, setTotal] = useState(0);
+  const [total, setTotal] = useState(0);
 
-    const handleOnChange = (position) => {
-        const updatedCheckedState = checkedState.map((item, index) =>
-          index === position ? !item : item
+  const handleOnChange = (position) => {
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : item
+    );
+
+    setCheckedState(updatedCheckedState);
+
+    const totalunFinishTask = updatedCheckedState.reduce(
+      (left, currentState, index) => {
+        if (currentState === true) {
+          return (left += 1);
+        }
+        return left;
+      },
+      0
+    );
+
+    setTotal(totalunFinishTask);
+  };
+  return (
+    <div className="task-view">
+      <p>{count - total} Unfinished Task</p>
+      {tasks.map(({ title, id }, index) => {
+        return (
+          <div className="task-items">
+            <div className="checkbox-container">
+              <input
+                type="checkbox"
+                id={`custom-checkbox-${index}`}
+                checked={checkedState[index]}
+                onChange={() => handleOnChange(index)}
+              />
+              <label>{title}</label>
+            </div>
+            <button onClick={() => deleteTask(id)}>
+              <img src={deleteIcon} alt="Delete" />
+            </button>
+          </div>
         );
-    
-        setCheckedState(updatedCheckedState);
-    
-        const totalunFinishTask = updatedCheckedState.reduce(
-          (left, currentState, index) => {
-            if (currentState === true) {
-              return left += 1;
-            }
-            return left;
-          },
-          0
-        );
-    
-        setTotal(totalunFinishTask);
-      };
-    return ( 
-        <div className="task-view">
-            
-            <p>{count - total}  Unfinished Task</p>
-            {tasks.map(({ title, id }, index) => {
-          return (
-              <div className="task-items">
-                  <div className="checkbox-container">
-                  <input
-                    type="checkbox"
-                    id={`custom-checkbox-${index}`}
-                    checked={checkedState[index]}
-                    onChange={() => handleOnChange(index)}
-                  />
-                  <label>{title}</label>
-                  </div>
-                  <button onClick={() => deleteTask(id)}><img src={deleteIcon} alt="Delete"/></button>
-              </div>
-          );
-        })}
-        </div>
-     );
-}
- 
+      })}
+    </div>
+  );
+};
+
 export default TaskList;
